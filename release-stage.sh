3dc4447c94267dd89f664351311270af77c5565b
Mikael Edgren <mikael.edgren@whatwedo.se>
2020-10-17 00:54:14 +0200

    Some more relase scripting

--- release-stage.sh
+++ release-stage.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
+source colors.sh
 
-echo "Select (pre-) release level";
-echo "1) Major"
-echo "2) Minor"
-echo "3) Patch (default)"
+echo ""
+echo "${bold}Select (pre-) release level";
+echo "${green}1) Major"
+echo "${cyan}2) Minor"
+echo "${magenta}3) Patch (default)${normal}"
 
 read ans
 case $ans in
     1 )  level=MAJOR ;;
     2 )  level=MINOR ;;
     3 )  level=PATCH ;;
-    "")  echo "default - Patch"; level=PATCH ;;
-    * )  echo "Whats that?"; exit ;;
+    "")  echo "${magenta}default - Patch"; level=PATCH ;;
+    * )  echo "${red}${bold}Bad input"; exit ;;
 esac
 
-echo "------------------------"
+echo "${darkblue}"
+echo "---------------------------"
 echo "Merge develop into stage..."
-echo "------------------------"
+echo "---------------------------"
+echo "${gray}"
 git checkout stage
+echo ""
 git merge develop --no-ff --no-edit
 
+echo "${darkblue}"
 echo "--------------------------"
 echo "Create ${level} release..."
 echo "--------------------------"
-standard-version --release-as ${LEVEL:-patch} --prerelease beta
+echo "${gray}"
+./node_modules/standard-version/bin/cli.js --release-as ${level} --prerelease beta
 
+echo "${darkblue}"
 echo "-------------------------"
 echo "Push release to Github..."
 echo "-------------------------"
+echo "${gray}"
 git push --follow-tags origin stage
 
+echo "${darkblue}"
 echo "-------------------------------"
 echo "Merge stage back into develop..."
 echo "-------------------------------"
+echo "${gray}"
 git checkout develop
+echo ""
 git merge stage --no-ff --no-edit
+echo ""
 git push origin develop
