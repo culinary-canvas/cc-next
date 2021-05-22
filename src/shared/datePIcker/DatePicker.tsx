c90eb5d4e3b178c2b0d953a2f3e664bc549208a6
Mikael Edgren <mikael.edgren@whatwedo.se>
2021-05-22 21:08:06 +0200

    #26 Tweaks to Date picker

--- src/shared/datePIcker/DatePicker.tsx
+++ src/shared/datePIcker/DatePicker.tsx
@@ -5,6 +5,7 @@ import ReactDatePicker, {
 } from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import sv from 'date-fns/locale/sv'
+import s from './DatePicker.module.scss'
 
 registerLocale('sv', sv)
 setDefaultLocale('sv')
@@ -28,14 +29,17 @@ export function DatePicker(props: Props) {
   return (
     <ReactDatePicker
       showTimeSelect
+      showWeekNumbers
       dateFormat="HH:mm EEEE d MMMM yyyy"
-      wrapperClassName={containerClassName}
       className={className}
       locale="sv"
       selected={selected}
       onChange={(d) => onChange(d as Date)}
       popperPlacement="bottom-end"
       disabled={disabled}
+      wrapperClassName={containerClassName}
+      popperClassName={s.popper}
+      calendarClassName={s.calendar}
     />
   )
 }
